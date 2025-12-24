195e0c5fc032919917e18d5555075c8320a245dc
toujouphilic <129611238+toujouphilic@users.noreply.github.com>
2025-12-23 21:40:44 -0500

    Create deploy-commands.js

---
+++ deploy-commands.js
@@ -0,0 +1,12 @@
+import 'dotenv/config';
+import { REST, Routes } from 'discord.js';
+import notif from './commands/notif.js';
+
+const rest = new REST({ version: '10' }).setToken(process.env.DISCORD_TOKEN);
+
+await rest.put(
+  Routes.applicationCommands(process.env.CLIENT_ID),
+  { body: [notif.data.toJSON()] }
+);
+
+console.log('✅ Slash commands deployed');
